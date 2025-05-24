640871b491c31ef757aea823bc4039aded675766
Gurjot30 <Gsingh3_be21@thapar.edu>
2025-05-24 11:25:01 +0530

    updates

--- JS/Script.js
+++ JS/Script.js
@@ -298,6 +298,19 @@ gsap.fromTo(".cta-button",
   }
 );
 
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        window.scrollTo({
+          top: target.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
 
 
 
